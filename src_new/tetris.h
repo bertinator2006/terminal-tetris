d7bf2334680ecc1207f7c843d8fc1aef674b4db9
Aayush Poudel <poudelaayush2006@gmail.com>
2026-09-20 14:56:10 +1000

    Renamed Game to TETRIS_Game for public interface

--- src_new/tetris.h
+++ src_new/tetris.h
@@ -7,28 +7,28 @@
 #define TETRIS_GRID_WIDTH 10
 
 typedef struct game *TETRIS_Game;
-typedef uint16_t TETRIS_PieceBitmap;
 
 typedef struct {
     uint8_t cells[TETRIS_GRID_HEIGHT * TETRIS_GIRD_WIDTH];
 } TETRIS_Grid;
 // Initialiser
 Game TETRIS_create_game(void);
-void TETRIS_destroy_game(Game g);
+void TETRIS_destroy_game(TETRIS_Game g);
 
 // Accessing board information
-TETRIS_Grid TETRIS_get_grid(Game g);
-TETRIS_PieceBitmap TETRIS_get_held_piece(Game g);
-int TETRIS_get_score(Game g);
+TETRIS_Grid TETRIS_get_grid(TETRIS_Game g);
+uint16_t TETRIS_get_held_piece(TETRIS_Game g);
+int TETRIS_get_score(TETRIS_Game g);
+uint8_t TETRIS_get_last_status(TETRIS_Game g);
 
 // Piece falling logic
-void TETRIS_next_frame(Game g);
-void TETRIS_soft_drop(Game g);
-void TETRIS_hard_drop(Game g);
-void TETRIS_clear_rows(Game g);
-void TETRIS_move_right(Game g);
-void TETRIS_move_left(Game g);
-void TETRIS_rotate(Game g);
-void TETRIS_hold_piece(Game g);
+void TETRIS_next_frame(TETRIS_Game g);
+void TETRIS_soft_drop(TETRIS_Game g);
+void TETRIS_hard_drop(TETRIS_Game g);
+void TETRIS_clear_rows(TETRIS_Game g);
+void TETRIS_move_right(TETRIS_Game g);
+void TETRIS_move_left(TETRIS_Game g);
+void TETRIS_rotate(TETRIS_Game g);
+void TETRIS_hold_piece(TETRIS_Game g);
 
 #endif
